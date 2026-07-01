6aea661bc281d89209e86605f90212f98331c338
Andrew M McCall <andrew.m.mccall@gmail.com>
2026-07-01 19:55:56 -0400

    fix events

--- themes/alienvibes/static/js/events.js
+++ themes/alienvibes/static/js/events.js
@@ -7,17 +7,17 @@ const eventData = {
     },
     {
       title: "Alien Invasion 2026",
-      date: "2026-07-017T00:00:00",
+      date: "2026-07-17T16:00:00",
       url: "/events/2026/fourth-of-july-2026/"
     },
     {
       title: "Swampy's Old Skool Game #9: Revenge of the Woodballer",
-      date: "2026-08-21T00:00:00",
+      date: "2026-08-21T16:00:00",
       url: "/events/2026/double-your-pleasure-double-the-swampy/"
     },
     {
       title: "Three Man Random Draw For Charity",
-      date: "2026-09-19T00:00:00",
+      date: "2026-09-19T16:00:00",
       url: "/events/2026/three-man-charity-sept-19/"
     },
   ]
